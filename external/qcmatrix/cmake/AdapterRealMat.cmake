a6ceae4b856a277b56f751a5b77477314275d057
Fic_Ohkubo <kouichi.ookubo@konicaminolta.com>
2019-01-31 15:04:06 +0900

    exchange operation 1

--- external/qcmatrix/cmake/AdapterRealMat.cmake
+++
@@ -1,7 +0,0 @@
-# Source codes for setting/getting the external real matrix
-SET(QCMATRIX_SRCS
-    ${QCMATRIX_SRCS}
-    ${LIB_QCMATRIX_PATH}/src/cmplx_mat/CmplxMatSetAdapterMat.c
-    ${LIB_QCMATRIX_PATH}/src/cmplx_mat/CmplxMatGetAdapterMat.c
-    ${LIB_QCMATRIX_PATH}/src/qcmat/QcMatSetAdapterMat.c
-    ${LIB_QCMATRIX_PATH}/src/qcmat/QcMatGetAdapterMat.c)
--- external/qcmatrix/cmake/c_adapter.cmake
+++
@@ -1,9 +0,0 @@
-# CMake file for C adapter
-#
-# Sets C compiler flags
-SET(LANG_C_HEADER "lib_matrix.h" CACHE STRING "Name of header file of the external C library.")
-SET(LANG_C_MATRIX "matrix_t" CACHE STRING "Name of external C matrix struct.")
-ADD_DEFINITIONS(-DLANG_C_HEADER=${LANG_C_HEADER})
-ADD_DEFINITIONS(-DLANG_C_MATRIX=${LANG_C_MATRIX})
-# Source codes of the adapter for the external C library
-SET(ADAPTER_SRCS ${LIB_QCMATRIX_PATH}/src/adapter/c_adapter.c)
